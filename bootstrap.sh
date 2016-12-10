982c2791f0eca7c3f7996fa2ab37040ff7effc34
Ryan Brooks <ryanbrooksis@gmail.com>
2016-12-10 12:22:48 +0000

    initial commit, Rails, RabbitMQ, Postgres

---
+++ bootstrap.sh
@@ -0,0 +1,47 @@
+# Based on https://github.com/rails/rails-dev-box/blob/master/bootstrap.sh
+
+# The output of all these installation steps is noisy. With this utility
+# the progress report is nice and concise.
+function install {
+    echo installing $1
+    shift
+    apt-get -y install "$@" >/dev/null 2>&1
+}
+
+echo adding swap file
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+
+echo updating package information
+apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
+apt-get -y update >/dev/null 2>&1
+
+install 'development tools' build-essential
+
+install Ruby ruby2.3 ruby2.3-dev
+update-alternatives --set ruby /usr/bin/ruby2.3 >/dev/null 2>&1
+update-alternatives --set gem /usr/bin/gem2.3 >/dev/null 2>&1
+
+echo installing Bundler
+gem install bundler -N >/dev/null 2>&1
+
+install Git git
+install Redis redis-server
+install RabbitMQ rabbitmq-server
+
+install PostgreSQL postgresql postgresql-contrib libpq-dev
+sudo -u postgres createuser --superuser ubuntu
+
+install 'Nokogiri dependencies' libxml2 libxml2-dev libxslt1-dev
+install 'Blade dependencies' libncurses5-dev
+install 'ExecJS runtime' nodejs
+
+# Needed for docs generation.
+locale-gen "en_GB.UTF-8"
+dpkg-reconfigure locales
+update-locale LANG=en_GB.UTF-8 LANGUAGE=en_GB.UTF-8 LC_ALL=en_GB.UTF-8
+
+echo 'all set, rock on!'
